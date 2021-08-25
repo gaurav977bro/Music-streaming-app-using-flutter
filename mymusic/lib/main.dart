2226a055843fa96802323aa43a547f0918c809b8
Gaurav Jha <jhagaurav667@gmail.com>
2021-08-25 23:24:13 +0530

    firebase changes

--- mymusic/lib/main.dart
+++ mymusic/lib/main.dart
@@ -1,17 +1,18 @@
+// @dart=2.9
+
+import 'package:firebase_core/firebase_core.dart';
 import "package:flutter/material.dart";
 import 'package:mymusic/screens/HomeScreen.dart';
 import 'package:mymusic/screens/LoginScreen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyMusic());
 }
 
 class MyMusic extends StatelessWidget {
-  const MyMusic({Key? key}) : super(key: key);
-
   @override
-
-  ///
   Widget build(BuildContext context) {
     return MaterialApp(
         theme: ThemeData(primarySwatch: Colors.green),
--- mymusic/lib/screens/LoginScreen.dart
+++ mymusic/lib/screens/LoginScreen.dart
@@ -1,186 +1,128 @@
-import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import "package:flutter/material.dart";
 import 'package:form_field_validator/form_field_validator.dart';
+
 import 'HomeScreen.dart';
 
-void main() {
-  runApp(Login());
-}
+enum MobilVerificationState { SHOW_PHONE_STATE, SHOW_OTP_STATE }
 
 class Login extends StatelessWidget {
-  const Login({Key? key}) : super(key: key);
+  final _formkey = GlobalKey<FormState>();
+
+  final currentState = MobilVerificationState.SHOW_PHONE_STATE;
+
+  final phoneController = TextEditingController();
+  final otpController = TextEditingController();
+
+  FirebaseAuth _auth = FirebaseAuth.instance;
+
+  getLogin(context) {
+    return Scaffold(
+        backgroundColor: Colors.blueGrey[900],
+        body: SafeArea(
+            child: SingleChildScrollView(
+                padding: EdgeInsets.only(top: 200, left: 20, right: 20),
+                child: Container(
+                    padding: EdgeInsets.all(10),
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.pink,
+                          offset: Offset(-10, -20),
+                          blurRadius: 20,
+                        )
+                      ],
+                      color: Color.fromRGBO(255, 255, 255, 1),
+                      borderRadius: BorderRadius.only(
+                          bottomRight: Radius.circular(30),
+                          topLeft: Radius.circular(30)),
+                    ),
+                    child: Form(
+                      key: this._formkey,
+                      child: Column(
+                        children: [
+                          SizedBox(height: 30),
+                          // TEXT FIELD FOR PHONE NO
+                          TextFormField(
+                              controller: phoneController,
+                              validator: MultiValidator([
+                                MinLengthValidator(10,
+                                    errorText: "should be 10 digits.")
+                              ]),
+                              decoration: InputDecoration(
+                                  border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(10)),
+                                  labelText: "Enter phone number")),
+                          SizedBox(height: 30),
+                          ElevatedButton(
+                              onPressed: () async {
+                                if (_formkey.currentState!.validate()) {
+                                   await _auth.verifyPhoneNumber(
+                                      phoneNumber: phoneController.text,
+                                       verificationCompleted: verificationCompleted,
+                                       verificationFailed: verificationFailed, codeSent: codeSent,
+                                       codeAutoRetrievalTimeout: codeAutoRetrievalTimeout)
+                                }
+                              },
+                              child: Text("Verify"))
+                        ],
+                      ),
+                    )))));
+  }
+
+  getOtp(context) {
+    return Scaffold(
+        backgroundColor: Colors.blueGrey[900],
+        body: SafeArea(
+            child: SingleChildScrollView(
+                padding: EdgeInsets.only(top: 200, left: 20, right: 20),
+                child: Container(
+                    padding: EdgeInsets.all(10),
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.pink,
+                          offset: Offset(-10, -20),
+                          blurRadius: 30,
+                        )
+                      ],
+                      color: Color.fromRGBO(255, 255, 255, 1),
+                      borderRadius: BorderRadius.only(
+                          bottomRight: Radius.circular(30),
+                          topLeft: Radius.circular(30)),
+                    ),
+                    child: Form(
+                      key: this._formkey,
+                      child: Column(
+                        children: [
+                          SizedBox(height: 30),
+                          // TEXT FIELD FOR PHONE NO
+                          TextFormField(
+                              controller: otpController,
+                              // validator: MultiValidator([
+                              //   MinLengthValidator(10,
+                              //       errorText: "should be 10 digits.")
+                              // ]),
+                              decoration: InputDecoration(
+                                  border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(10)),
+                                  labelText: "Enter OTP")),
+                          SizedBox(height: 30),
+                          ElevatedButton(
+                              onPressed: () {}, child: Text("Verify"))
+                        ],
+                      ),
+                    )))));
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      color: Colors.blueGrey[900],
-      child: SafeArea(
-          child: SingleChildScrollView(
-              child: Column(
-        children: [
-          Padding(
-            padding: EdgeInsets.all(30),
-          ),
-          Container(padding: EdgeInsets.all(20), child: FormContent()),
-        ],
-      ))),
-    );
-  }
-}
-
-class FormContent extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-        height: 600,
-        width: 500,
-        padding: EdgeInsets.only(left: 10, right: 10),
-        decoration: BoxDecoration(
-            border: Border.all(color: Colors.pink),
-            color: Colors.white,
-            boxShadow: [
-              BoxShadow(
-                  offset: Offset(-15, -20),
-                  color: Colors.pinkAccent,
-                  blurRadius: 30)
-            ],
-            borderRadius: BorderRadius.only(
-              bottomLeft: Radius.circular(50),
-              topRight: Radius.circular(50),
-            )),
-        child: Column(
-          children: [
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
-            ),
-            Icon(Icons.my_library_music, color: Colors.pink, size: 50),
-            SizedBox(
-              height: 30,
-            ),
-            Text("Login",
-                style: TextStyle(
-                    fontSize: 35,
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold,
-                    fontStyle: FontStyle.italic)),
-            SizedBox(
-              height: 30,
-            ),
-            TextFormFields(),
-            SizedBox(
-              height: 30,
-            ),
-          ],
-        ));
-  }
-}
-
-// TEXT FORM FOR EMAIL AND PASSOWRD
-class TextFormFields extends StatefulWidget {
-  @override
-  _TextFormFieldsState createState() => _TextFormFieldsState();
-}
-
-class _TextFormFieldsState extends State<TextFormFields> {
-  int _count = 0;
-
-  bool _tapped = false;
-
-  final _formKey = GlobalKey<FormState>();
-
-  verify(BuildContext context) {
-    if (_formKey.currentState!.validate()) {
-      Navigator.push(context, MaterialPageRoute(builder: (context) => Home()));
-      setState(() {});
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.only(
-              topRight: Radius.circular(30), bottomLeft: Radius.circular(30)),
-          border: Border.all(
-              color: Colors.pink, style: BorderStyle.solid, width: 4)),
-      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-      child: Form(
-        key: _formKey,
-        child: Column(
-          children: [
-            SizedBox(
-              height: 50,
-            ),
-            TextFormField(
-                autovalidate: true,
-                validator: MultiValidator([
-                  RequiredValidator(errorText: "Required*"),
-                  EmailValidator(errorText: "Invalid Email"),
-                ]),
-                decoration: InputDecoration(
-                  filled: true,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  prefixIcon: Icon(Icons.email, color: Colors.pink),
-                  hintStyle: TextStyle(color: Colors.green),
-                  hintText: "Enter your email",
-                  labelStyle: TextStyle(color: Colors.green),
-                  labelText: "Email",
-                )),
-            SizedBox(
-              height: 30,
-            ),
-            TextFormField(
-              obscureText: _tapped ? false : true,
-              decoration: InputDecoration(
-                filled: true,
-                prefixIcon: _tapped
-                    ? Icon(Icons.lock_open_outlined, color: Colors.pink)
-                    : Icon(Icons.lock, color: Colors.pink),
-                suffix: GestureDetector(
-                    child: _tapped
-                        ? Icon(Icons.visibility_off, color: Colors.pink)
-                        : Icon(Icons.visibility, color: Colors.pink),
-                    onTap: () {
-                      _count++;
-
-                      if (_count % 2 != 0) {
-                        _tapped = true;
-                      } else {
-                        _tapped = false;
-                      }
-                      setState(() {});
-                    }),
-                hintStyle: TextStyle(color: Colors.green),
-                labelStyle: TextStyle(color: Colors.green),
-                hintText: "Enter you password",
-                labelText: "Password",
-                border:
-                    OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-              ),
-              autovalidate: true,
-              validator: MultiValidator([
-                RequiredValidator(errorText: "Required*"),
-                MinLengthValidator(5, errorText: "Atleast 6 characters"),
-                PatternValidator(r'(?=.*?[#?!@$%^&*-])',
-                    errorText:
-                        'passwords must have at least one special character')
-              ]),
-            ),
-            SizedBox(height: 50),
-            ElevatedButton(
-                child: Text("Login"),
-                onPressed: () => verify(context),
-                style: ElevatedButton.styleFrom(minimumSize: Size(100, 40)))
-          ],
-        ),
-      ),
+    return Scaffold(
+      body: currentState == MobilVerificationState.SHOW_PHONE_STATE
+          ? getLogin(context)
+          : getOtp(context),
     );
   }
 }
